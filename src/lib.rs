a7030d5a2afdd93bdba8455a5acf3860a478a829
Junyi Xiang <xjy03@hust.edu.cn>
2025-06-05 17:09:36 +0800

    fix: safer functions and more user friendly error message.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 #![allow(unused)]
 
 pub mod todolist {
+    use super::error::MyError;
     use ansi_term::Style;
     use serde::{Deserialize, Serialize};
     use std::fs::File;
-    use std::io::{BufReader, BufWriter};
+    use std::io::{self, BufReader, BufWriter};
     use std::path::Path;
 
     #[derive(Deserialize, Serialize)]
@@ -37,12 +38,25 @@ pub mod todolist {
         pub fn add(&mut self, todo: Todo) {
             self.list.push(todo);
         }
-        pub fn done(&mut self, index: usize) {
-            self.list[index - 1].state = States::Done;
+        pub fn done(&mut self, index: usize) -> Result<(), MyError> {
+            if index < 1 || index > self.list.len() {
+                Err(MyError::IndexError {
+                    message: String::from("Index out of bounds for the todo list"),
+                })
+            } else {
+                self.list[index - 1].state = States::Done;
+
+                Ok(())
+            }
         }
-        pub fn show(&self) {
+        pub fn show(&self) -> Result<(), MyError> {
             let strike_through_style = Style::new().strikethrough();
             let mut index: usize = 1;
+            // exit the function when no todo
+            if self.list.is_empty() {
+                println!("No todo for now. Remember to add new task!");
+                return Ok(());
+            }
             for todo in &self.list {
                 let text = &format!("{}. {}", index, todo.term);
                 match todo.state {
@@ -51,44 +65,118 @@ pub mod todolist {
                 }
                 index += 1;
             }
+
+            Ok(())
         }
-        pub fn read_json(&mut self, file_path: String) {
-            let file = File::open(file_path).unwrap();
+        pub fn backup(&self, file_path: String) {
+            todo!();
+        }
+        pub fn remove(&mut self, index: usize) {
+            todo!();
+        }
+        pub fn edit(&mut self, index: usize, new_term: String) {
+            todo!();
+        }
+        pub fn open(&mut self, file_path: String) {
+            todo!();
+        }
+        pub fn read_json<P: AsRef<Path>>(&mut self, file_path: P) -> Result<(), MyError> {
+            let file = File::open(&file_path)?;
             let reader = BufReader::new(file);
-            self.list = serde_json::from_reader(reader).unwrap();
+            self.list = serde_json::from_reader(reader)?;
+
+            Ok(())
         }
-        pub fn write_json(&self, file_path: String) {
-            let file = File::create(file_path).unwrap();
+        pub fn write_json<P: AsRef<Path>>(&self, file_path: P) -> Result<(), MyError> {
+            let file = File::create(file_path)?;
             let writer = BufWriter::new(file);
-            serde_json::to_writer_pretty(writer, &self.list);
+            serde_json::to_writer_pretty(writer, &self.list)?;
+
+            Ok(())
         }
     }
 }
 
 pub mod cli {
+    use super::error::MyError;
     use super::todolist::{Todo, TodoList};
-    use clap::Parser;
+    use clap::{Parser, Subcommand};
+    use std::io;
 
     #[derive(Parser, Debug)]
     #[command(version, about, long_about = None)]
     pub struct Args {
-        #[arg(short, long, num_args(0..))]
-        add: Option<String>,
-
-        #[arg(short, long, num_args(0..))]
-        done: Option<usize>,
-
-        #[arg(short, long, num_args(0..))]
-        show: Option<String>,
+        #[command(subcommand)]
+        pub command: Option<Commands>,
     }
 
-    pub fn run(todolist: &mut TodoList, args: Args) {
-        if let Some(term) = args.add {
-            todolist.add(Todo::new(term.clone()));
+    #[derive(Subcommand, Debug)]
+    pub enum Commands {
+        Add { term: String },
+        Done { index: usize },
+        Show,
+    }
+
+    pub fn run(todolist: &mut TodoList, args: Args) -> Result<(), MyError> {
+        match args.command {
+            Some(Commands::Add { term }) => {
+                todolist.add(Todo::new(term));
+            }
+            Some(Commands::Done { index }) => {
+                todolist.done(index)?;
+            }
+            _ => (),
         }
-        if let Some(index) = args.done {
-            todolist.done(index);
+
+        todolist.show()?;
+
+        Ok(())
+    }
+}
+
+pub mod error {
+    use std::fmt;
+    use std::fs::File;
+    use std::io;
+
+    use clap::builder::Str;
+
+    #[derive(Debug)]
+    pub enum MyError {
+        IoError { message: String },
+        JSONError { message: String },
+        IndexError { message: String },
+    }
+
+    impl fmt::Display for MyError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                MyError::IndexError { message } => write!(f, "{message}"),
+                MyError::IoError { message } => write!(f, "{message}"),
+                MyError::JSONError { message } => write!(f, "{message}"),
+            }
         }
-        todolist.show();
+    }
+
+    impl From<serde_json::Error> for MyError {
+        fn from(error: serde_json::Error) -> MyError {
+            use serde_json::error::Category;
+            match error.classify() {
+                Category::Io => MyError::IoError {
+                    message: error.to_string(),
+                },
+                _ => MyError::JSONError {
+                    message: error.to_string(),
+                },
+            }
+        }
+    }
+
+    impl From<io::Error> for MyError {
+        fn from(error: io::Error) -> MyError {
+            MyError::IoError {
+                message: error.to_string(),
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 #![allow(unused)]
 
+use std::process;
+
 use clap::Parser;
 use cli_todo::cli::{Args, run};
+use cli_todo::error::MyError;
 use cli_todo::todolist::{Todo, TodoList};
 
 fn main() {
     let mut todo_list: TodoList = TodoList { list: vec![] };
 
-    todo_list.read_json(String::from("todo.json"));
+    todo_list
+        .read_json(String::from("todo.json"))
+        .unwrap_or_else(|err| {
+            println!("Problem reading todo history from json: {}", err);
+            process::exit(1);
+        });
 
     let args = Args::parse();
 
-    run(&mut todo_list, args);
+    run(&mut todo_list, args).unwrap_or_else(|err| {
+        println!("Problem dealing with commands: {}", err);
+        process::exit(1);
+    });
 
-    todo_list.write_json(String::from("todo.json"));
+    todo_list
+        .write_json(String::from("todo.json"))
+        .unwrap_or_else(|err| {
+            println!("Problem writing todo history to json: {}", err);
+            process::exit(1);
+        });
 }
